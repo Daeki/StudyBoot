7dd2f1918c74639d921665591dc2f9e4d435b035
daeki <kimdaeki@gmail.com>
2022-10-25 17:46:31 +0900

    1

--- src/main/java/com/iu/home/member/MemberService.java
+++ src/main/java/com/iu/home/member/MemberService.java
@@ -2,8 +2,10 @@ package com.iu.home.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class MemberService {
 	@Autowired
 	private MemberMapper memberMapper;
@@ -11,7 +13,15 @@ public class MemberService {
 	public int setAdd(MemberVO memberVO)throws Exception{
 		int result = memberMapper.setAdd(memberVO);
 		
+		if(result<1) {
+			throw new Exception();
+		}
+		
 		result = memberMapper.setMemberRole(memberVO);
+		
+		if(result<1) {
+			throw new Exception();
+		}
 		
 		return result;
 	}
